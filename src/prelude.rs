2417c0a28190d0e83f6296b539c34e583b771667
AlessioZanga <alessio.zanga@outlook.it>
2022-01-20 19:49:30 +0100

    Update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,13 +1,14 @@
-// Re-export macros
+// Re-export macros.
 pub use crate::{Adj, An, Ch, De, Ne, Pa, E, V};
 
-// Re-export algorithms
-pub use crate::algorithms::{BFS, DFS};
+// Re-export algorithms.
+pub use crate::algorithms::*;
 
-// Re-export graphs
+// Re-export graphs.
 pub use crate::graphs::{DiGraph, DiGraphl, Graph, Graphl};
 
+// Re-export traits.
 pub use crate::traits::*;
 
-// Re-export types
+// Re-export types.
 pub use crate::types::*;
