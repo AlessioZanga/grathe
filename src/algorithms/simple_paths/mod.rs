d2d72b67121162db5226af61cf631a272cc1c200
AlessioZanga <alessio.zanga@outlook.it>
2022-05-04 14:10:43 +0200

    Add `AllPotentiallyDirectedPaths`

--- src/algorithms/simple_paths/mod.rs
+++ src/algorithms/simple_paths/mod.rs
@@ -4,6 +4,9 @@ pub use all_circle_paths::AllCirclePaths;
 mod all_discriminating_paths;
 pub use all_discriminating_paths::AllDiscriminatingPaths;
 
+mod all_potentially_directed_paths;
+pub use all_potentially_directed_paths::AllPotentiallyDirectedPaths;
+
 mod all_simple_cycles;
 pub use all_simple_cycles::AllSimpleCycles;
 
--- tests/algorithms/mod.rs
+++ tests/algorithms/mod.rs
@@ -1,4 +1,5 @@
 pub mod all_circle_paths;
 pub mod all_discriminating_paths;
+pub mod all_potentially_directed_paths;
 pub mod simple_paths;
 pub mod traversal;
